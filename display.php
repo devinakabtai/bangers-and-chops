39ec27ccf457224f777d70cd54576626c081c03c
devinakabtai <devinmurraydev@gmail.com>
2021-12-11 10:48:16 -0700

    Colorado has non beautiful output

--- display.php
+++ display.php
@@ -5,21 +5,19 @@
   <meta charset="UTF-8">
   <title>Directory Contents</title>
   <link rel="stylesheet" href=".style.css">
-  <script src=".sorttable.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
 
   <div id="container">
   
-    <h1>Directory Contents</h1>
+    <h1>Colorado Property Records</h1>
     
     <table class="sortable">
       <thead>
         <tr>
           <th>Filename</th>
-          <th>Type</th>
-          <th>Size <small>(bytes)</small></th>
           <th>Date Modified</th>
         </tr>
       </thead>
@@ -91,15 +89,65 @@
           // Cleans up . and .. directories 
           if($name=="."){$name=". (Current Directory)"; $extn="&lt;System Dir&gt;";}
           if($name==".."){$name=".. (Parent Directory)"; $extn="&lt;System Dir&gt;";}
+
+          $input = $namehref;
+          $output = explode('_', $input);
+          //output 0 = city
+          //output 1 = number
+          //output 2 = street
+          //output 3 = street suffix ++ must be cleaned
+          $jumper = explode('.', $output[3]);
+          $output[3] = $jumper[0];
           
           // Print 'em
-          print("
+          print
+          ("
+
+          <div class='container mt-1'>
+        <div class='row d-flex justify-content-center'>
+            <div class='card' style='width: 18rem;'>
+
+                <!--<embed src='Colorado/2200_BALSAM_DR-pr.pdf' type='application/pdf' width='100%' height='600px' />
+                    Use this for the popup so they're easily viewable.
+                    LOL fuck u i did it with variables first
+                -->
+
+                <div class='card-body'>
+                <!--<embed class='card-img-top' src='Colorado/$namehref' type='image/jpg' alt='card cap'/>-->
+                <h5 class='card-title'>$output[0]</h5>
+                <p class='card-text'>$output[1] $output[2] $output[3] <br> last edited: $modtime</p>
+                <a href='#' class='btn btn-primary'>Save</a><a href='#' class='btn btn-primary'>Sign</a>
+                </div>
+            </div>
+            <div class='card' style='width: 18rem;'>
+                <!--<embed src='Colorado/2200_BALSAM_DR-pr.pdf' type='application/pdf' width='100%' height='600px' />
+                    Use this for the popup so they're easily viewable.
+                -->
+                <div class='card-body'>
+                <h5 class='card-title'>$class</h5>
+                <p class='card-text'>$name <br> last edited: $modtime</p>
+                <a href='#' class='btn btn-primary'>Save</a><a href='#' class='btn btn-primary'>Sign</a>
+                </div>
+            </div>
+            <div class='card' style='width: 18rem;'>
+                <!--<embed src='Colorado/2200_BALSAM_DR-pr.pdf' type='application/pdf' width='100%' height='600px' />
+                    Use this for the popup so they're easily viewable.
+                -->
+                <div class='card-body'>
+                <h5 class='card-title'>$class</h5>
+                <p class='card-text'>$name <br> last edited: $modtime</p>
+                <a href='#' class='btn btn-primary'>Save</a><a href='#' class='btn btn-primary'>Sign</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
           <tr class='$class'>
             <td><a href='./$namehref'>$name</a></td>
-            <td><a href='./$namehref'>$extn</a></td>
-            <td><a href='./$namehref'>$size</a></td>
             <td sorttable_customkey='$timekey'><a href='./$namehref'>$modtime</a></td>
-          </tr>");
+          </tr>
+          
+          ");
           }
         }
       ?>
